9e19bbda458c2f6c2849865e7c024835945c6d3c
Joshua Leung <joshuajazleung@gmail.com>
2018-11-23 12:47:30 -0800

    edit package.json

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 // import createDebug from 'debug';
-import webpack from 'webpack';
+
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import historyApiFallback from 'connect-history-api-fallback';
