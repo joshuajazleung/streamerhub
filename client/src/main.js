12902f7bbcc2177780b814247409b8e84939896d
Joshua Leung <joshuajazleung@gmail.com>
2018-11-23 16:17:55 -0800

    add google analytics

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,8 @@ import '@babel/polyfill';
 import Vue from 'vue';
 import './plugins/vuetify';
 import Vuetify from 'vuetify';
+import VueAnalytics from 'vue-analytics';
+
 import App from './App.vue';
 import './assets/css/index.css';
 import router from './router';
@@ -9,6 +11,9 @@ import router from './router';
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify);
+Vue.use(VueAnalytics, {
+    id: 'UA-129732371-1'
+});
 
 new Vue({
     render: h => h(App),
